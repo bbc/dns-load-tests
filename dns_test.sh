34a80bc1ee3829e9b88a61b0302b639c5943533b
aidylewis <adrian_lewis@hushmail.com>
2016-04-21 17:43:03 +0100

    more metrics

--- dns_test.sh
+++ dns_test.sh
@@ -2,8 +2,10 @@
 
 # Example
 # sh dns_test.sh bind "www.bbc.co.uk A" 2 2 
+# sh dns_test.sh lbnamed "www.bbc.net.uk A" 2 2
 
-rm -f /tmp/*log.*
+tmplog=/tmp/*.log.*
+rm -f tmplog
 
 if [ "$1" = "bind" ]
 then
@@ -27,5 +29,14 @@ do
   dnsperf -s $ip -l $duration -q 20 -d "$HOME/query" > /tmp/$1.log.$count &
 done
 wait
-grep "Queries per second" /tmp/$1.log.* | 
-  perl -p -e 's/[\r\n]+$//; s/^.* (\d+)\..*$/$1/; push(@val, $_); $sum += $_; $_=""; END { print "$sum = " . join("+",@val) . "\n";}'
+qps=$(grep "Queries per second" $tmplog | 
+perl -p -e 's/[\r\n]+$//; s/^.* (\d+)\..*$/$1/; push(@val, $_); $sum += $_; $_=""; END { print "$sum = " . join("+",@val) . "\n";}')
+
+min=$(grep -h 'Average Latency' $tmplog | sed 's/,//g'| awk '{print $6}')
+max=$(grep -h 'Average Latency' $tmplog | sed -e 's/)//g' | awk '{print $8}')
+av=$(grep -h 'Average Latency' $tmplog | awk '{print $4}')
+
+echo qps: $qps
+echo min: $min
+echo max: $max
+echo av:  $av
