280ef63b056be700736933665a96929c6042289f
aidylewis <adrian_lewis@hushmail.com>
2016-05-10 13:05:23 +0100

    added timestamps

--- tests/dig_test.sh
+++ tests/dig_test.sh
@@ -3,9 +3,13 @@
 rm -f dig.log
 
 start=`date +%s`
-secs=60
+secs=1200
+
+echo start: `date` > dig.log
 
 while [ $(( $(date +%s) - secs )) -lt $start ]; do
   dig +tcp -f data/axfr_domains.txt  @132.185.154.12 | egrep '^;; Query time:|^;; XFR size:' >> dig.log
   sleep 0.1
 done
+
+echo end: `date` >> dig.log
